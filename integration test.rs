5869ff68f0c01979d2d842fe37cccd0ae8f96590
StevenMuanigo <elalmisemre72@gmail.com>
2025-11-06 19:58:24 +0300

    Create integration test.rs

---
+++ integration test.rs
@@ -0,0 +1,13 @@
+use transformer_forge::*;
+
+#[tokio::test]
+async fn test_model_loading() {
+    // Integration test placeholder
+    assert!(true);
+}
+
+#[tokio::test]
+async fn test_inference_pipeline() {
+    // Integration test placeholder
+    assert!(true);
+}
